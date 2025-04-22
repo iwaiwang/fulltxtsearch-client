061cb7991df3537af4e42109c30896533e4682c3
iwaiwang <iwai.wang@gmail.com>
2025-04-22 15:48:43 +0800

    加了其他检索条件

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ opensearch_client = opensearchpy.OpenSearch(
 
 # PDF 文件存储路径
 PDF_DIR = '/Users/john/Data/projects/es_test/test/pdf_files'  # 请替换为实际PDF存储路径
-
+INDEX_NAME='medical_records'
 
 # 日志配置
 
@@ -54,6 +54,9 @@ def index():
 @app.route('/api/search', methods=['GET'])
 def search():
     query = request.args.get('query', '')
+    file_type = request.args.get('file_type', '')  # 新增：文件类型参数
+    patient_name = request.args.get('patient_name', '')  # 新增：患者姓名参数
+    hospital_id = request.args.get('hospital_id', '')
     page = int(request.args.get('page', 1))  # 当前页，从1开始
     size = int(request.args.get('size', 10))  # 每页条数，默认10
 
@@ -64,27 +67,66 @@ def search():
     from_idx = (page - 1) * size
 
     # OpenSearch 查询
-    search_body = {
+    # 构建 OpenSearch 查询
+    search_query = {
         'query': {
-            'match_phrase': {
-                '页内容': query
+            'bool': {
+                'must': [
+                    {
+                        'match_phrase': {
+                            '页内容': {
+                                'query': query,
+                                #'analyzer': 'ik_smart'
+                            }
+                        }
+                    }
+                ],
+                'filter': [] # 初始化 filter 列表
             }
         },
+        'from': from_idx,
+        'size': size,
         'highlight': {
             'fields': {
                 '页内容': {}
             },
             'pre_tags': ['<strong>'],
             'post_tags': ['</strong>']
-        },
-        'from': from_idx,  # 分页起始位置
-        'size': size       # 每页返回条数
+        }
     }
-    logger.info(f"Searching for query: {search_body}")
+    
+    # 如果指定了文件类型，添加 filter
+    if file_type:
+        search_query['query']['bool']['filter'].append(
+            {
+                'term': {
+                    '文件类型.keyword': file_type
+                }
+            }
+        )
+
+    if patient_name:
+        search_query['query']['bool']['filter'].append(
+            {
+                'term': {
+                    '患者名.keyword': patient_name
+                }
+            }
+        )
+    if hospital_id:
+            search_query['query']['bool']['filter'].append(
+                {
+                    'term': {
+                        '住院号': hospital_id
+                    }
+                }
+            )
+
+    logger.info(f"Searching for query: {search_query}")
     try:
         response = opensearch_client.search(
-            index='medical_records',
-            body=search_body
+            index=INDEX_NAME,
+            body=search_query
         )
 
         results = []
@@ -92,6 +134,11 @@ def search():
             highlight_text = hit.get('highlight', {}).get('页内容', [''])[0] if hit.get('highlight') else hit['_source']['页内容']
             results.append({
                 'id': hit['_id'],
+                'patient': hit['_source']['患者名'],
+                'hospital_id': hit['_source']['住院号'],
+                'admission_date': hit['_source']['入院时间'],
+                'discharge_date': hit['_source']['出院时间'],
+                'doc_type': hit['_source']['文件类型'],
                 'filename': hit['_source']['文件名称'],
                 'page': hit['_source']['页号'],
                 'text': highlight_text
@@ -113,14 +160,33 @@ def search():
         logger.error(f"Error occurred: {str(e)}")
         return jsonify({'error': str(e)}), 500
 
+
 @app.route('/api/pdf', methods=['GET'])
 def get_pdf():
+    # 获取文件名
     filename = request.args.get('filename')
     logger.info(f"Requested PDF: {filename}")
     file_path = os.path.join(PDF_DIR, filename)
     if os.path.exists(file_path):
         return send_file(file_path, mimetype='application/pdf')
     return jsonify({'error': 'PDF not found'}), 404
+
+
+@app.route('/api/file_types', methods=['GET'])
+def get_file_types():
+    #获取opensearch中所有文件类型
+    response = opensearch_client.search(index=INDEX_NAME, body={
+        'aggs': {
+            'by_type': {
+                'terms': {
+                    'field': '文件类型.keyword',
+                    'size': 100
+                }
+            }
+        }
+    })
+    file_types = [bucket['key'] for bucket in response['aggregations']['by_type']['buckets']]
+    return jsonify(file_types)
 
 if __name__ == '__main__':
     app.run(debug=True,port=5001)
